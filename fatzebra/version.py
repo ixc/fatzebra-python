beb1cdc1a2f3fa86fc097a487f0d72525af144f5
Tai Lee <real.human@mrmachine.net>
2016-04-04 12:51:00 +1000

    Get version from git.

--- fatzebra/version.py
+++ fatzebra/version.py
@@ -1,1 +1,4 @@
-VERSION = "0.0.6" # edit with care, see setup.py
+# from setuptools_scm import get_version
+
+# VERSION = get_version(root=__file__, version_scheme='post-release')
+VERSION = '0.0.6'  # See: https://bitbucket.org/hpk42/devpi/issues/332/devpi-generated-tarball-not-100-compatible
--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='fatzebra',
-    version=open('fatzebra/version.py').read().split('"')[1],
+    use_scm_version={'version_scheme': 'post-release'},
+    setup_requires=['setuptools_scm'],
     description='Fat Zebra Python Library',
     long_description=open("README.txt").read(),
     author='Fat Zebra',
     author_email='support@fatzebra.com.au',
     url='https://www.fatzebra.com.au/',
     packages=['fatzebra'],
-    install_requires=['requests >= 0.14.2', 'simplejson'],
+    install_requires=['requests >= 0.14.2', 'setuptools_scm', 'simplejson'],
     test_suite='test',
 )
